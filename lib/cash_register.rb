853b76ca9477a65e9453fb06107abb8589034701
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-08 13:47:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister 
+  attr_accessor :total, :discount
+  
+  def initialize(discount=nil)
+    @total = 0
+    @discount = discount 
+  end 
+  
+  def add_item(title, price, quantity=1)
+   sum = price * quantity
+    self.total += sum 
+  end 
 
+  def apply_discount
+    
+    if @discount != 0 || nil 
+      @discount = @discount.to_f / @total * 10 
+      sum = @discount * @total
+      @total = @total - sum
+        return "After the discount, the total comes to $#{@total.to_i}." 
+    elsif @discount == 0 || nil 
+      return "There is no discount to apply."
+    end
+  end
+end 
