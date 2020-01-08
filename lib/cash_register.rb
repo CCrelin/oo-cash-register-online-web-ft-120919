3cd71d4a2d6305df41845c510f384a1cbdb982c1
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-08 14:41:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,8 +16,8 @@ class CashRegister
     if @discount != 0 || nil 
      disc = @discount.to_f / @total * 10 
       sum = disc * @total
-      @total = @total - sum
-        return "After the discount, the total comes to $#{@total.to_i}." 
+      @total = @total.to_i - sum.to_i
+        return "After the discount, the total comes to $#{@total}." 
     elsif @discount == 0 || nil 
       return "There is no discount to apply."
     end
