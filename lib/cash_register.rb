6c739c683d81990a22376de0d76814bb3f682888
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-08 16:20:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end 
   
   def add_item(title, price, quantity=1)
-   @items << quantity.times {|q| 
+    quantity.times {|q| @items << title}
    sum = price * quantity
     self.total += sum 
   end 
