fc4de1488841704cca55afc3b7e77737972f0fdf
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-08 14:57:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,10 +12,10 @@ class CashRegister
   end 
 
   def apply_discount
-    if discount != 0 && total
+    if discount != 0 || nil
       self.total = (self.total * ((100.0 - discount.to_f)/100)).to_i
-      return "After the discount, the total comes to $#{@total}."
-    else
+      return "After the discount, the total comes to $#{self.total}."
+    elsif discount = 0 || nil
       return "There is no discount to apply."
     end
   end
