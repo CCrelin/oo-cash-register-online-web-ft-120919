3b2c8d0669dd164237b7b2c25bcfc541e305fcbe
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-08 15:17:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ class CashRegister
   
   def items
     array =[]
-    array << add_item(title, price, quantity=1).first   
+    array << add_item(title, price, quantity=1)   
     array 
   end
   
