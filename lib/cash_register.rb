0ae68c5b2a708f91bb03df6464d63846bd70da7f
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-08 18:28:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     quantity.times {|q| @items << title}
     binding.pry
     sum = price * quantity
-    @void_last_transaction -= price
+    @void_last_transaction -= 
     self.total += sum 
     
     
